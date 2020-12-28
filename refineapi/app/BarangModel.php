488193d2a5f18261763ea9f76c199c9a10c6c15a
Matahari Kridha <matakridha@users.noreply.github.com>
2020-12-28 15:53:02 +0700

    Latihan Refine API

---
+++ refineapi/app/BarangModel.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class BarangModel extends Model
+{
+    protected $table = 'stuff';
+    //aa
+    protected $primaryKey = 'kode_barang';
+    //aaa
+}
